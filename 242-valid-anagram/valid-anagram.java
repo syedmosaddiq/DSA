905e03cc44906a0a01758c976f4eec498b917fcd
Mosaddique Khalil <khalilmosaddique@gmail.com>
2026-08-19 14:43:19 +0530

    Time: 17 ms (16.67%) | Memory: 46.1 MB (50.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,27 +1,24 @@
- class Solution {
- public boolean isAnagram(String s, String t) {
-
-       
- if (s.length() != t.length()) {
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
             return false;
         }
-
-     int[] count= new int[26];
-
-        
-        for (int i = 0; i < s.length(); i++) {
-        count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
+        for (char ch : t.toCharArray()) {
+            if(!map.containsKey(ch)){
                 return false;
             }
-        }
+              if (map.get(ch) == 0) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
 
-    return true;
+        }
+        return true;
+
     }
 }
-  
