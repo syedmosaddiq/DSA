bffd58d794fcb08d17609ad9c9bd9c0d73bdedcd
Mosaddique Khalil <khalilmosaddique@gmail.com>
2026-08-19 13:15:21 +0530

    Time: 19 ms (47.22%) | Memory: 111.2 MB (5.03%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int num: nums){
+            if(hm.containsKey(num)){
+                return true;
+            }
+            hm.put(num,1);
+        }
+        return false;
+    }
+}
